33cd8253c8de40683e086406172396a075b08ed3
JRA-zachpennington <90287368+JRA-zachpennington@users.noreply.github.com>
2024-11-02 00:43:27 -0500

    database

---
+++ rg-chat-toolkit-api-cs/Data/DataMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using rg_chat_toolkit_api_cs.Data.Models;
+
+namespace rg_chat_toolkit_api_cs.Data;
+
+public static class DataMethods
+{
+
+    //public static readonly Guid PRODUCER_TOKEN_SYSTEM = Guid.Parse("00000000-0000-0000-0000-000000000000");
+
+    public static Prompt? Prompt_Get(Guid tenantID, string name)
+    {
+        var db = RGDatabaseContextFactory.Instance.CreateDbContext();
+        return db.Prompts
+            .Where(p =>
+                p.TenantId == tenantID
+                && p.Name == name
+            )
+            .FirstOrDefault();
+    }
+
+}
---
+++ rg-chat-toolkit-api-cs/Data/RGContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.IO;
+using rg_chat_toolkit_api_cs.Data.Models;
+using rg_chat_toolkit_api_cs.Config;
+
+namespace rg_chat_toolkit_api_cs.Data;
+
+internal class RGDatabaseContextFactory : IDesignTimeDbContextFactory<RgToolkitContext>
+{
+    public static RGDatabaseContextFactory Instance = new RGDatabaseContextFactory();
+
+    public RgToolkitContext CreateDbContext()
+    {
+        return CreateDbContext([]);
+    }
+
+    public RgToolkitContext CreateDbContext(string[] args)
+    {
+        // Step 2: Retrieve the connection string from user secrets
+        var connectionString = ConfigurationHelper.GetConnectionString("RG-Toolkit");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("Connection string not found in configuration.");
+        }
+
+        // Step 3: Configure DbContext
+        var optionsBuilder = new DbContextOptionsBuilder<RgToolkitContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new RgToolkitContext(optionsBuilder.Options);
+    }
+}
