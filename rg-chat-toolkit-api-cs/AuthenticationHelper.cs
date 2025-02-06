d7958d6e58f7df86d541974cd139e5a93a375010
JRA-zachpennington <zach.pennington@jordanRiverAdvisors.com>
2025-02-05 21:47:46 -0600

    jwt authorization

---
+++ rg-chat-toolkit-api-cs/AuthenticationHelper.cs
@@ -0,0 +1,58 @@
+﻿using rg_chat_toolkit_api_cs.Chat;
+using rg_chat_toolkit_api_cs.Data.Models;
+using rg_chat_toolkit_cs.Chat;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace rg_chat_toolkit_api_cs;
+
+public static class AuthenticationHelper
+{
+    const string CLAIMS_KEY_USER_ROLE_RGADMIN = "rg:user_role";
+    const string CLAIMS_VALUE_USER_ROLE_RGADMIN_ADMIN = "administrator";
+
+    public static void Authorize(IIdentity? identity, RequestBase request)
+    {
+        // Verify that the TenantID in the HTTP Header value matches the TenantID in the request
+        bool isAuthorizedTenant = false;
+        var httpContext = new HttpContextAccessor().HttpContext;
+        var httpHeaderTenantID = httpContext?.Request?.Headers["TenantID"].ToString();
+        var requestTenantID = request.TenantID.ToString();
+        if (httpHeaderTenantID == requestTenantID)
+        {
+            isAuthorizedTenant = true;
+        }
+        if (!isAuthorizedTenant)
+        {
+            throw new UnauthorizedAccessException("Authorization: Unauthorized TenantID.");
+        }
+
+        bool isRgAdmin = false;
+        isRgAdmin = ((ClaimsIdentity)identity)?
+            .Claims?.Where(claim => claim.Type.EndsWith(CLAIMS_KEY_USER_ROLE_RGADMIN))?
+            .Any(claim => claim.Value == CLAIMS_VALUE_USER_ROLE_RGADMIN_ADMIN) ?? false;
+        bool isAuthorizedUser = false;
+        if (isRgAdmin)
+        {
+            isAuthorizedUser = true;
+        }
+        else
+        {
+            // Verify that "sub" in the JWT token matches the UserID in the request
+            const string CLAIMS_KEY_USER_ID = "custom:producer_token";
+            string? claimUserID = ((ClaimsIdentity)identity)?
+                .Claims?.Where(claim => claim.Type == CLAIMS_KEY_USER_ID)?
+                .FirstOrDefault()?
+                .Value;
+            var requestedUserID = request.UserID.ToString();
+            if (claimUserID?.ToLower() == requestedUserID?.ToLower())
+            {
+                isAuthorizedUser = true;
+            }
+        }
+        if (!isAuthorizedUser)
+        {
+            throw new UnauthorizedAccessException("Authorization: Unauthorized UserID.");
+        }
+    }
+}
--- rg-chat-toolkit-api-cs/Program.cs
+++ rg-chat-toolkit-api-cs/Program.cs
@@ -1,10 +1,16 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using rg_chat_toolkit_api_cs.Cache;
 using rg_chat_toolkit_cs.Cache;
 using rg_chat_toolkit_cs.Chat;
 using rg_integration_abstractions.Embedding;
+using System.Net;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace rg_chat_toolkit_api_cs
@@ -20,11 +26,151 @@ namespace rg_chat_toolkit_api_cs
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Description = "Bearer Authentication with JWT Token",
+                    Type = SecuritySchemeType.Http
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Id = "Bearer",
+                                    Type = ReferenceType.SecurityScheme
+                                }
+                            },
+                            new List<string>()
+                        }
+                    });
+            });
 
             // RG
             builder.Services.AddSingleton<IRGEmbeddingCache, RGCache>();
             builder.Services.AddSingleton<EmbeddingBase, RGEmbedding>();
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Headers.ContainsKey("TenantID"))
+                            {
+                                context.HttpContext.Items["TenantID"] = context.Request.Headers["TenantID"].ToString();
+                            }
+                            return Task.CompletedTask;
+                        },
+                        OnChallenge = context =>
+                        {
+                            bool isTenantIDSpecified = context.HttpContext.Items.ContainsKey("TenantID");
+                            bool isTenantIDValid = false;
+                            if (isTenantIDSpecified)
+                            {
+                                var tenantId = context.HttpContext.Items["TenantID"]?.ToString();
+                                Guid tenantIdGuid;
+                                if (Guid.TryParse(tenantId, out tenantIdGuid))
+                                {
+                                    try
+                                    {
+                                        LookupValidIssuer(tenantIdGuid);
+                                        isTenantIDValid = true;
+                                    }
+                                    catch (SecurityTokenInvalidIssuerException)
+                                    {
+                                        isTenantIDValid = false;
+                                    }
+                                }
+                            }
+
+                            const string MESSAGE_TENANT_ID_MISSING = "Authentication: Missing TenantID.";
+                            const string MESSAGE_TENANT_ID_INVALID = "Authentication: Invalid TenantID.";
+
+                            var message = isTenantIDSpecified ? (isTenantIDValid ? MESSAGE_TENANT_ID_INVALID : MESSAGE_TENANT_ID_MISSING) : MESSAGE_TENANT_ID_MISSING;
+
+                            // Skip the default logic.
+                            context.HandleResponse();
+
+                            // Return a custom response.
+                            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                            //context.Response.ContentType = "application/json";
+                            //var result = JsonConvert.SerializeObject(new
+                            //{
+                            //    StatusCode = context.Response.StatusCode,
+                            //    Message = message
+                            //});
+                            return context.Response.WriteAsync(message);
+                        }
+                    };
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        IssuerSigningKeyResolver = (s, securityToken, identifier, parameters) =>
+                        {
+                            var httpContext = new HttpContextAccessor().HttpContext;
+                            if (httpContext != null && httpContext.Request.Headers.ContainsKey("TenantID"))
+                            {
+                                var tenantId = httpContext.Request.Headers["TenantID"].ToString();
+                                Guid tenantIdGuid;
+                                if (Guid.TryParse(tenantId, out tenantIdGuid))
+                                {
+                                    parameters.ValidIssuer = LookupValidIssuer(tenantIdGuid);
+                                }
+                            }
+
+                            var json = new WebClient().DownloadString(parameters.ValidIssuer + "/.well-known/jwks.json");
+                            var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(json).Keys;
+                            return (IEnumerable<SecurityKey>)keys;
+                        },
+
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+
+                        // Dynamically validate issuer using TenantID
+                        IssuerValidator = (issuer, securityToken, parameters) =>
+                        {
+                            var httpContext = new HttpContextAccessor().HttpContext;
+                            if (httpContext != null && httpContext.Items.ContainsKey("TenantID"))
+                            {
+                                var tenantId = httpContext.Items["TenantID"]?.ToString();
+                                Guid tenantIdGuid;
+                                if (!Guid.TryParse(tenantId, out tenantIdGuid))
+                                {
+                                    throw new SecurityTokenInvalidIssuerException("Authentication: Malformed TenantID");
+                                }
+
+                                var expectedIssuer = LookupValidIssuer(tenantIdGuid); // Dynamically get issuer
+                                if (issuer != expectedIssuer)
+                                {
+                                    throw new SecurityTokenInvalidIssuerException($"Invalid Issuer for Tenant {tenantId}");
+                                }
+                            }
+                            else
+                            {
+                                throw new SecurityTokenInvalidIssuerException("Authentication: Missing TenantID");
+                            }
+                            return issuer; // Return the validated issuer
+                        }
+                    };
+                });
+            builder.Services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
 
             var app = builder.Build();
 
@@ -38,8 +184,6 @@ namespace rg_chat_toolkit_api_cs
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
-            //app.UseAuthorization();
 
             app.UseExceptionHandler(exceptionHandlerApp =>
             {
@@ -70,9 +214,27 @@ namespace rg_chat_toolkit_api_cs
             .AllowAnyMethod()
             .AllowAnyHeader());
 
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static string LookupValidIssuer(Guid? tenantId)
+        {
+            // 902544DA-67E6-4FA8-A346-D1FAA8B27A08
+            // -> https://cognito-idp.us-east-2.amazonaws.com/us-east-2_CrGQj2ghJ/.well-known/jwks.json
+
+            Guid TENANTID_TILLEY = new Guid("902544DA-67E6-4FA8-A346-D1FAA8B27A08");
+            if (tenantId == TENANTID_TILLEY)
+            {
+                return "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_CrGQj2ghJ";
+            }
+            else
+            {
+                throw new SecurityTokenInvalidIssuerException("Unknown TenantID");
+            }
         }
     }
 }
